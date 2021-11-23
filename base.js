56edfa01478277de9b86ee647c4857e3a0c47b09
Zijie Zhao <zzjas98@gmail.com>
2021-11-23 01:52:32 -0800

    base converter

--- base.js
+++ base.js
@@ -1,7 +1,12 @@
-function parseBase10(s) {
-    const parsed = parseInt(s, 10);
+function parseBase(s, base) {
+    const parsed = parseInt(s, base);
     return isNaN(parsed) ? 0 : parsed;
+}
+
+function parseBase10(s) {
+    return parseBase(s, 10);
 }
 
 const ID = (s) => document.getElementById(s);
 const CLASS = (s) => document.getElementsByClassName(s);
+const TAG = (s) => document.getElementsByTagName(s);
