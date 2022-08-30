5470c503162de82b5f97a6e752e08651ac39538f
Zijie Zhao <zzjas98@gmail.com>
2022-08-29 23:33:44 -0500

    fix splitbills

--- base.js
+++ base.js
@@ -8,6 +8,10 @@ function parseBase10(s) {
     return parseBase(s, 10);
 }
 
+function safeParseFloat(s) {
+    return parseFloat(s) | 0;
+}
+
 const ID = (s) => document.getElementById(s);
 const CLASS = (s) => document.getElementsByClassName(s);
 const TAG = (s) => document.getElementsByTagName(s);
